1ba0686b58a077e48349c6673a16a31b9521e8c0
AtharvaJoshi21 <joshi.atharva92@hotmail.com>
2020-08-12 21:25:26 +0530

    Initial commiit

---
+++ CaseConversion/CaseConversion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace CaseConversion
+{
+    /// <summary>
+    /// Program to Convert Camel case (SampleInput) to snake case (sample_input)
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputStr = Console.ReadLine();
+
+            Console.WriteLine(Solve(inputStr));
+
+            Console.ReadLine();
+        }
+
+        static string Solve(string s)
+        {
+            string outStr = string.Empty;
+            outStr = s[0].ToString().ToLower();
+            for (int i = 1; i < s.Length; i++)
+            {
+                var charAscii = (int)s[i];
+                if (charAscii >= 65 && charAscii <= 90)
+                    outStr += string.Format("_{0}", s[i].ToString().ToLower());
+                else
+                    outStr += s[i].ToString();
+            }
+
+            return outStr;
+        }
+    }
+}
---
+++ Transpose2DArray/Transpose2DArray/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Transpose2DArray
+{
+    /// <summary>
+    /// Program to demonstrate Transpose a matrix of given dimensions
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rowsNCols = Console.ReadLine().Split(' ');
+
+            int rows = Convert.ToInt32(rowsNCols[0]);
+            int cols = Convert.ToInt32(rowsNCols[1]);
+            int[,] inputArr = new int[rows,cols];
+
+            for (int i = 0; i < rows; i++)
+            {
+                var rowData = Console.ReadLine().Split(' ');
+                for (int j = 0; j < cols; j++)
+                {
+                    inputArr[i, j] = Convert.ToInt32(rowData[j]);
+                }
+            }
+
+            Transpose(inputArr, rows, cols);
+
+            Console.ReadLine();
+        }
+
+        static void Transpose(int[,] inputArr, int rowNum, int colNum)
+        {
+            for (int i = 0; i < colNum; i++)
+            {
+                for (int j = 0; j < rowNum; j++)
+                {
+                    Console.Write(string.Format("{0} ", inputArr[j, i]));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
